86e81be88e156f95b4321666c69bf876092bdc77
Gorczu <gorczu@gmail.com>
2019-07-10 18:06:03 +0200

    Update YouTubeAPIServiceHelper.cs

--- CommonControls/Helpers/YouTubeAPIServiceHelper.cs
+++ CommonControls/Helpers/YouTubeAPIServiceHelper.cs
@@ -8,7 +8,7 @@ namespace CommonControls.Helpers
 {
     public class YouTubeAPIServiceHelper
     {
-        public static readonly string API_KEY = "AIzaSyD0PZiNzYdahSXU3QSN3YgiVsB45w5MQWA";
-        public static readonly string APP_NAME = "second-flame-190610";
+        public static readonly string API_KEY = "**";
+        public static readonly string APP_NAME = "**";
     }
 }
